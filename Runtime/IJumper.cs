e7eeb885b5840c1a0b821b1a874eed90e45f3921
CHM <56677134+chocola-mint@users.noreply.github.com>
2023-08-01 23:04:31 +0800

    Added comments for IJumper

--- Runtime/IJumper.cs
+++ Runtime/IJumper.cs
@@ -5,12 +5,27 @@ using TriInspector;
 
 namespace CHM.Actstar
 {
+    /// <summary>
+    /// Interface for components that implement some kind of jumping behavior.
+    /// </summary>
     public interface IJumper
     {
+        /// <summary>
+        /// Can the object jump at the moment?
+        /// </summary>
         public bool CanJump { get; }
+        /// <summary>
+        /// Is the object currently jumping?
+        /// </summary>
         public bool IsJumping { get; }
 
-        public void CancelJump();
+        /// <summary>
+        /// Makes the object start jumping upwards.
+        /// </summary>
         public void Jump();
+        /// <summary>
+        /// Aborts the ongoing jump. This is usually used to indicate short-hops.
+        /// </summary>
+        public void CancelJump();
     }
 }
